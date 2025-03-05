4e38676ecef4a2418274e86355991fc0f6eacabe
Natã Ribeiro Barbosa <95133483+natanribeirobarbosa@users.noreply.github.com>
2025-03-05 00:37:52 -0300

    Update firebase_config.js

--- firebase_config.js
+++ firebase_config.js
@@ -1,9 +1,1 @@
-firebaseConfig = {
-    "apiKey": "AIzaSyCE_c5zAcXLNxk3174Ckk8tzW2u5xkblqM",
-    "authDomain": "horarios-e6e7e.firebaseapp.com",
-    "projectId": "horarios-e6e7e",
-    "storageBucket": "horarios-e6e7e.firebasestorage.app",
-    "messagingSenderId": "136180553611",
-    "appId": "1:136180553611:web:af261802c026405706418e",
-    "measurementId": "G-84G79HLNQP"
-  }
+
